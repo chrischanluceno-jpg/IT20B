00ce37dbf18376979b4ec31467f8a6a417b4c77a
chrischanluceno-jpg <chrischanluceno@gmail.com>
2025-11-18 08:17:49 +0800

    Initial commit

---
+++ cs.js
@@ -0,0 +1,14 @@
+function legalAge(age){
+
+        if( age >= 18){
+            console.log("You are an Adult");   
+        } else{
+            console.log("You are not an Adult");
+        }
+}
+
+legalAge(69);
+
+function computeGrade(grade){
+    
+}
