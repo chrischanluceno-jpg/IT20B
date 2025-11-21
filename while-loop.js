22753a74e2bd21451bcf2fd02d9f7a649c44754c
chrischanluceno-jpg <chrischanluceno@gmail.com>
2025-11-21 08:37:07 +0800

    Added while-loop.js

---
+++ while-loop.js
@@ -0,0 +1,62 @@
+/*
+
+
+*/
+
+
+//Asc
+/*
+i =1;
+
+while (i<=5){
+    console.log(i);
+    i++;
+}
+
+//Online Asc
+/*
+output = "";
+i =1;
+
+while (i<=5){
+    output += output;
+    console.log(output);
+    i++;
+}
+console.log(output)
+
+
+/*
+output = "";
+i =1;
+
+while (i<=5){
+    output += output;
+    console.log(output);
+    i++;
+}
+console.log(output)
+*/
+
+/*
+output = "";
+i =5;
+
+while (i<=1){
+    output += i + " ";
+    console.log(output);
+    i--;
+}
+console.log(output);
+*/
+
+arr=[4,2,3];
+output = " ";
+i=0;
+
+while(i<=arr.length){
+    output += arr[i] + " ";
+    i++;
+}
+
+console.log(output);
