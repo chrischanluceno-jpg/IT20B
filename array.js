d0063931a11d1faf7050659c13450eb7ee10c2c3
chrischanluceno-jpg <chrischanluceno@gmail.com>
2025-11-25 08:54:50 +0800

    Added initial methods for DSA/array.js

--- array.js
+++ array.js
@@ -1,13 +1,14 @@
 class Array{
 
-    constructor(inittialData = []){
+    constructor(initialData = []){
         this.data = {};
-        this.length = 0; 
-    //Assign element ti index
-    for(let i=0; i<inittialData.length; i++){
-        this.data[i] = initialData[i];
-        this.length++;
-    }
+        this.length = 0;
+
+        //Assign element to index
+        for(let i=0; i<initialData.length; i++){
+            this.data[i] = initialData[i];
+            this.length++;
+        }
     }
 
     traverse(){
@@ -16,38 +17,14 @@ class Array{
         for(let i=0; i<this.length; i++){
             output += this.data[i] + " ";
         }
-        
+
         console.log(output);
     }
 
-    insert(index, element){
-        //Correcting statement
-        if(index <0 || index > this.length){
-            console.log("Invalid Index");
-            return;
-        }
-
-        //Shifting Condition
-        for(let i= index; i>length; i++){
-            this.data[i] = this.data[i-1];
-        }
-    
-        //After Shifting
-       delete this.data[this.length-1];
-       this.length--;
-
-
-    }
-
 }
-arr1 = nw Array([4,2,3]);
 
-arr1.traverse();
-arr1.insert(0,9);
-arr1.traverse();
+//
 
-arr1.delete(1);
-arr1.traverse();
+arr1 = new Array([4,2,3]);
 
-arr1.update(2,5);
 arr1.traverse();
